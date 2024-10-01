ec3748a6cf420f0ec128bdb38c821ad1ded1f552
FeHari <alexandregomesap202@gmail.com>
2024-10-01 20:59:40 -0300

    FH.lua

--- FH.lua
+++ FH.lua
@@ -1,39 +1,100 @@
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
+local Window = Rayfield:CreateWindow({
+    Name = "Legends Of Speed Gui",
+    LoadingTitle = "Exploiter Heaven",
+    LoadingSubtitle = "by badnanax",
+    ConfigurationSaving = {
+        Enabled = false,
+        FolderName = nil,
+        FileName = "legendsofspeedexploiter"
+    },
+    Discord = {
+        Enabled = true,
+        Invite = "NbjDSTyCvj",
+        RememberJoins = false
+    },
+    KeySystem = false,
+    KeySettings = {
+        Title = "Check the Discord server",
+        Subtitle = "Key System",
+        Note = "Join the Discord server for more information",
+        FileName = "fun_keysystem",
+        SaveKey = false,
+        GrabKeyFromSite = true,
+        Key = {"https://pastebin.com/raw/ULATYMKJ"}
+    }
+})
 
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/FeHariHub/FarmNovo/main/LibUI.lua"))()
+local MainTab = Window:CreateTab("Auto Farm", nil)
+local MainSection = MainTab:CreateSection("Main")
 
--- Window
-local Window, MainGUI = Library:CreateWindow("TissueSense | Universal")
+Rayfield:Notify({
+    Title = "Enjoy!",
+    Content = "Be the best in the game :)",
+    Duration = 5,
+    Image = nil,
+    Actions = {
+        Ignore = {
+            Name = "Thanks!",
+            Callback = function()
+                print("The user tapped Thanks!")
+            end
+        }
+    },
+})
 
--- Tabs
-local VisualsTab = Window:CreateTab("Visuals")
-local AimTab = Window:CreateTab("Aim")
-local MiscTab = Window:CreateTab("Misc")
-
-local VisualsGroupBox = VisualsTab:CreateGroupbox("Visuals", "Left")
-local AimGroupBox = AimTab:CreateGroupbox("Aim", "Left")
-local MiscGroupBox = MiscTab:CreateGroupbox("Misc", "Left")
-
-local LegendsOfSpeedXP = MiscGroupBox:CreateButton('XP Farm', function()
-    while wait() do
-        local A_1 = "collectOrb"
-        local A_2 = "Red Orb"
-        local A_3 = "City"
-        local Event = game:GetService("ReplicatedStorage").rEvents.orbEvent
-        Event:FireServer(A_1, A_2, A_3)
-
-        for i,v in pairs(game:GetService("Workspace").Hoops:GetChildren()) do
-            v.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+-- Auto Steps Toggle
+local ToggleSteps = MainTab:CreateToggle({
+    Name = "Auto Steps",
+    CurrentValue = false,
+    Flag = "steps1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+    Callback = function(Value)
+        getgenv().AutoSteps = Value -- Set AutoSteps to the current toggle value
+        while getgenv().AutoSteps do
+            local args = {
+                [1] = "collectOrb",
+                [2] = "Orange Orb",
+                [3] = "City"
+            }
+            game:GetService("ReplicatedStorage").rEvents.orbEvent:FireServer(unpack(args))
+            wait() -- You might want to adjust the wait time to control the frequency
         end
     end
-end)
+})
 
-local LegendsOfSpeedRebirth = MiscGroupBox:CreateButton("Auto Rebirth", function ()
-    while wait() do
- 
-    local A_1 = "rebirthRequest"
-    local Event = game:GetService("ReplicatedStorage").rEvents.rebirthEvent
-    Event:FireServer(A_1)
-
+-- Auto Gems Toggle
+local ToggleGems = MainTab:CreateToggle({
+    Name = "Auto Gems",
+    CurrentValue = false,
+    Flag = "gems1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+    Callback = function(Value)
+        getgenv().AutoGems = Value -- Set AutoGems to the current toggle value
+        while getgenv().AutoGems do
+            local args = {
+                [1] = "collectOrb",
+                [2] = "Gem",
+                [3] = "City"
+            }
+            game:GetService("ReplicatedStorage").rEvents.orbEvent:FireServer(unpack(args))
+            wait() -- You might want to adjust the wait time to control the frequency
+        end
     end
-end)
+})
+
+-- Auto Rebirth Toggle
+local ToggleRebirth = MainTab:CreateToggle({
+    Name = "Auto Rebirth",
+    CurrentValue = false,
+    Flag = "rebirths1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+    Callback = function(Value)
+        getgenv().AutoRebirth = Value -- Set AutoRebirth to the current toggle value
+        while getgenv().AutoRebirth do
+            local args = {
+                [1] = "rebirthRequest"
+            }
+            game:GetService("ReplicatedStorage").rEvents.rebirthEvent:FireServer(unpack(args))
+            wait() -- You might want to adjust the wait time to control the frequency
+        end
+    end
+})
