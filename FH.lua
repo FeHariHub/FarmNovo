5f2c8d3daf913d995191c7cb0c18f2960a4a2a20
FeHari <alexandregomesap202@gmail.com>
2024-10-06 21:23:31 -0300

    FH.lua

--- FH.lua
+++ FH.lua
@@ -1,94 +1,181 @@
-local Luminosity = loadstring(game:HttpGet("https://raw.githubusercontent.com/FeHariHub/FarmNovo/main/LibUI.lua", true))()
+local FeHari = loadstring(game:HttpGet("https://raw.githubusercontent.com/FeHariHub/FarmNovo/main/LibUI.lua", true))()
 
-local Window = Luminosity.new("FeHari Hub", "v1.0.0", 4370345701)
+Hello, and welcome to this documentation document! This will briefly cover all the necessary steps, you need to create a nice UI. :)
+The main variable you will be using is called ("library").
 
-local Tab1 = Window.Tab("Início", 100789040568622)
-local Folder = Tab1.Folder("Options", "A bunch of options you can use")
-Folder.Button("Button", function()
-print("Button Clicked")
-end)
-Folder.Switch("Switch", function(Status)
-print("Switch Triggered: " .. tostring(Status))
-end)
-Folder.Toggle("Toggle", function(Status)
-print("Toggle Triggered: " .. tostring(Status))
-end)
-Folder.Toggle("Toggle", function(Status)
-print("Toggle Triggered: " .. tostring(Status))
-end)
-Folder.TextBox("Textbox", "Placeholder", function(Text)
-print("TextBox Triggered: " .. Text)
-end)
-Tab1.Folder("Lipsum Expanded", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eu mollis urna, quis feugiat tellus. Integer ut ligula sodales, sodales ipsum ut, imperdiet ipsum. In aliquet quam et venenatis pulvinar. Nullam fermentum porta felis sit amet interdum. Sed tristique fringilla mollis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam quis tempus mauris, nec ultrices metus. Suspendisse mi urna, accumsan at nisi a, tristique porta libero. Integer lobortis elementum lacus cursus consectetur. Morbi mauris ante, posuere at malesuada et, tristique non ipsum. Proin vitae purus pretium, convallis est vitae, dignissim leo. Praesent nec felis vitae.")
+Function Map:
+library: { 
+	AddWindow(<string> title, <table> options [optional])
+	-> {
+		AddTab(<string> title)
+		-> {
+			AddLabel(<string> text)
+			AddButton(<string> title, <function> callback)
+			AddTextBox(<string> title, <function> callback, <table> options [clear = true]) -- callback args: <string> text
+			AddSwitch(<string> title, <function> callback) -- callback args: <boolean> statement
+			-> {
+				Set(<boolean> statement)
+			}
+			AddSlider(<string> title, <function> callback, <table> options [min = 0, max = 100, readonly = false]) -- callback args: <number> number selected
+			-> {
+				Set(<number> number)
+			}
+			AddKeybind(<string> title, <function> callback, <table> options [standard = Enum.KeyCode.RightShift])
+			-> {
+				SetKeybind(<Enum> keycode object)
+			}
+			AddDropdown(<string> title, <function> callback) -- callback args: <string> selected object name
+			-> {
+				Add(<string> title) -- adds object with ("title") to the dropdown list
+				-> {
+					Remove() -- removes the object from the dropdown list
+				}
+			}
+			AddColorPicker(<function> callback) -- callback args: <Color3> color selected
+			-> {
+				Set(<Color3> color)
+			}
+			AddConsole(<table> options [y = 200 (height of the console), full = false (fills out entire tab), source = "Lua" (or "Logs"), readonly = true])
+			-> {
+				Get() -- returns input
+				Set(<string> input) -- sets input
+				Log(<string> message) -- adds a message to the console
+			}
+			AddHorizontalAlignment() -- creates empty ui-element, will align elements horizontally (buttons only)
+			-> {
+				AddButton(...) -- same as other AddButton, except it will be placed horizontally
+			}
+			AddFolder(<string> title)
+			-> {
+				-- returns the same as AddTab, except functions used from this return will be added into the folder.
+			}
 
-local Cheat = Tab1.Cheat("Options", "A bunch of options you can use", function(Status)
-print("Cheat Triggered: " .. tostring(Status))
-end)
-Cheat.Button("Button", function()
-print("Button Clicked")
-end)
-Cheat.Switch("Switch", function(Status)
-print("Switch Triggered: " .. tostring(Status))
-end)
-Cheat.Toggle("Toggle", function(Status)
-print("Toggle Triggered: " .. tostring(Status))
-end)
-Cheat.Toggle("Toggle", function(Status)
-print("Toggle Triggered: " .. tostring(Status))
-end)
-Cheat.TextBox("Textbox", "Placeholder", function(Text)
-print("TextBox Triggered: " .. Text)
-end)
+			Show() -- function to open tab
+		}
+	}
 
-Tab1.Cheat("Lipsum Expanded", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eu mollis urna, quis feugiat tellus. Integer ut ligula sodales, sodales ipsum ut, imperdiet ipsum. In aliquet quam et venenatis pulvinar. Nullam fermentum porta felis sit amet interdum. Sed tristique fringilla mollis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam quis tempus mauris, nec ultrices metus. Suspendisse mi urna, accumsan at nisi a, tristique porta libero. Integer lobortis elementum lacus cursus consectetur. Morbi mauris ante, posuere at malesuada et, tristique non ipsum. Proin vitae purus pretium, convallis est vitae, dignissim leo. Praesent nec felis vitae.", function(Status)
-print("Cheat Triggered: " .. tostring(Status))
-end)
+	FormatWindows() -- will place the current windows nicely. this function is recommended to use after UI setup.
+}
 
--- Tab 2 --
-local Tab2 = Window.Tab("Tab 2", 6022668945)
-local Folder = Tab2.Folder("Options", "A bunch of options you can use")
-Folder.Button("Button", function()
-print("Button Clicked")
-end)
-Folder.Switch("Switch", function(Status)
-print("Switch Triggered: " .. tostring(Status))
-end)
-Folder.Toggle("Toggle", function(Status)
-print("Toggle Triggered: " .. tostring(Status))
-end)
-Folder.Toggle("Toggle", function(Status)
-print("Toggle Triggered: " .. tostring(Status))
-end)
-Folder.TextBox("Textbox", "Placeholder", function(Text)
-print("TextBox Triggered: " .. Text)
-end)
-Tab2.Folder("Lipsum Expanded", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eu mollis urna, quis feugiat tellus. Integer ut ligula sodales, sodales ipsum ut, imperdiet ipsum. In aliquet quam et venenatis pulvinar. Nullam fermentum porta felis sit amet interdum. Sed tristique fringilla mollis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam quis tempus mauris, nec ultrices metus. Suspendisse mi urna, accumsan at nisi a, tristique porta libero. Integer lobortis elementum lacus cursus consectetur. Morbi mauris ante, posuere at malesuada et, tristique non ipsum. Proin vitae purus pretium, convallis est vitae, dignissim leo. Praesent nec felis vitae.")
+To create a new window, simply use the following function:
+local window = library:AddWindow(<string> title, <table> options) -- Keep in mind that the options argument is not necessary, if you just leave it nil, it will use the default ui_options table, which is located at the top.
 
-local Cheat = Tab2.Cheat("Options", "A bunch of options you can use", function(Status)
-print("Cheat Triggered: " .. tostring(Status))
-end)
-Cheat.Button("Button", function()
-print("Button Clicked")
-end)
-Cheat.Switch("Switch", function(Status)
-print("Switch Triggered: " .. tostring(Status))
-end)
-Cheat.Toggle("Toggle", function(Status)
-print("Toggle Triggered: " .. tostring(Status))
-end)
-Cheat.Toggle("Toggle", function(Status)
-print("Toggle Triggered: " .. tostring(Status))
-end)
-Cheat.TextBox("Textbox", "Placeholder", function(Text)
-print("TextBox Triggered: " .. Text)
-end)
+Notice how I assign the window to a variable ("window"). This is because the function returns a table consisting of more functions, which you can use on this window.
 
-Tab2.Cheat("Lipsum Expanded", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eu mollis urna, quis feugiat tellus. Integer ut ligula sodales, sodales ipsum ut, imperdiet ipsum. In aliquet quam et venenatis pulvinar. Nullam fermentum porta felis sit amet interdum. Sed tristique fringilla mollis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam quis tempus mauris, nec ultrices metus. Suspendisse mi urna, accumsan at nisi a, tristique porta libero. Integer lobortis elementum lacus cursus consectetur. Morbi mauris ante, posuere at malesuada et, tristique non ipsum. Proin vitae purus pretium, convallis est vitae, dignissim leo. Praesent nec felis vitae.", function(Status)
-print("Cheat Triggered: " .. tostring(Status))
-end)
+To add a tab to our window, we can use the following function:
+local tab = window:AddTab()
+This function will also return a table with functions.
+To make this tab *show* as first thing, we will add this:
+tab:Show()
 
-game:GetService("UserInputService").InputBegan:Connect(function(Input)
-if Input.KeyCode == Enum.KeyCode.F then
-Window:Toggle()
-end
+Now we can begin adding stuff to this tab!
+
+[Examples]:
+AddLabel:
+```
+tab:AddLabel("Hello World!")
+```
+
+AddButton:
+```
+tab:AddButton("Give ...", function()
+	print("Gave ... !")
 end)
+```
+
+AddTextBox:
+```
+tab:AddTextBox("Teleport to Player", function(text)
+	teleport_to(game:GetService("Players"):FindFirstChild("text") or game:GetService("Players").LocalPlayer)
+end)
+```
+
+AddSwitch:
+```
+local switch = tab:AddSwitch("God Mode", function(bool)
+	toggle_god_mode(bool)
+end)
+switch:Set(true)
+```
+
+AddSlider:
+```
+local slider = tab:AddSlider("WalkSpeed", function(p)
+	setwalkspeed(p)
+end, {
+	["min"] = 16,
+	["max"] = 100,
+})
+slider:Set(16)
+```
+
+AddKeybind:
+```
+local keybind = tab:AddKeybind("Toggle", function(obj)
+	ui_options.toggle_key = obj
+end, {
+	["standard"] = Enum.KeyCode.RightShift,
+})
+```
+
+AddDropdown:
+```
+local dropdown = tab:AddDropdown("Teleport to Location", function(text)
+	if text == "Mars" then
+		teleport(CFrame.new(...))
+	elseif ...
+	end
+end)
+local mars = dropdown:Add("Mars")
+local earth = dropdown:Add("Earth")
+local not_a_planet = dropdown:Add("Iridocyclitis")
+not_a_planet:Remove()
+```
+
+AddColorPicker:
+```
+tab:AddLabel("Theme color")
+local cp = tab:AddColorPicker(function(color)
+	ui_options.main_color = color
+end)
+```
+
+AddFolder:
+```
+local folder = tab:AddFolder()
+folder:AddSwitch()
+folder:AddLabel("Woo! I'm inside a folder!")
+
+local folder2 = folder:AddFolder()
+folder2:AddLabel("I'm inside *two* folders :smirk:")
+```
+
+[Adding Options]:
+The only two element-functions taking options right now is "AddSlider" and "AddTextBox".
+To set options, simply add a table as the last argument, like this:
+```
+tab:AddSlider("WalkSpeed", function(x)
+	setwalkspeed(x)
+end, {
+	["min"] = 16, -- default : 0
+	["max"] = 100, -- default : 100
+})
+```
+
+Now for AddTextBox example:
+```
+tab:AddTextBox("Epic", function(text)
+	print("TextBox : " .. text)
+end, {
+	["clear"] = false, -- default : true
+})
+```
+
+AddConsole example:
+```
+tab:AddConsole({
+	["y"] = 210,
+	["readonly"] = false,
+	["source"] = "Lua",
+})
+```
