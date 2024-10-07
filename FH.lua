543fa4ce89f6da2e64060bb9908841da01cb4f6b
FeHari <alexandregomesap202@gmail.com>
2024-10-06 21:00:39 -0300

    FH.lua

--- FH.lua
+++ FH.lua
@@ -1,49 +1,94 @@
-local UILib = loadstring(game:HttpGet('https://raw.githubusercontent.com/FeHariHub/FarmNovo/main/LibUI.lua'))()
-local Window = UILib.new("Grand Piece Online", game.Players.LocalPlayer.UserId, "Buyer")
-local Category1 = Window:Category("Main", "http://www.roblox.com/asset/?id=8395621517")
-local SubButton1 = Category1:Button("Combat", "http://www.roblox.com/asset/?id=8395747586")
-local Section1 = SubButton1:Section("Section", "Left")
-Section1:Button({
-    Title = "Kill All",
-    ButtonName = "KILL!!",
-    Description = "kills everyone",
-    }, function(value)
-    print(value)
+local Luminosity = loadstring(game:HttpGet("https://raw.githubusercontent.com/FeHariHub/FarmNovo/main/UserInterface/Luminosity.lua", true))()
+
+local Window = Luminosity.new("Luminosity UI", "v1.0.0", 4370345701)
+
+local Tab1 = Window.Tab("Tab 1", 6026568198)
+local Folder = Tab1.Folder("Options", "A bunch of options you can use")
+Folder.Button("Button", function()
+print("Button Clicked")
 end)
-Section1:Toggle({
-    Title = "Auto Farm Coins",
-    Description = "Optional Description here",
-    Default = false
-    }, function(value)
-    print(value)
+Folder.Switch("Switch", function(Status)
+print("Switch Triggered: " .. tostring(Status))
 end)
-Section1:Slider({
-    Title = "Walkspeed",
-    Description = "",
-    Default = 16,
-    Min = 0,
-    Max = 120
-    }, function(value)
-    print(value)
+Folder.Toggle("Toggle", function(Status)
+print("Toggle Triggered: " .. tostring(Status))
 end)
-Section1:ColorPicker({
-    Title = "Colorpicker",
-    Description = "",
-    Default = Color3.new(255,0,0),
-    }, function(value)
-    print(value)
+Folder.Toggle("Toggle", function(Status)
+print("Toggle Triggered: " .. tostring(Status))
 end)
-Section1:Textbox({
-    Title = "Damage Multiplier",
-    Description = "",
-    Default = "",
-    }, function(value)
-    print(value)
+Folder.TextBox("Textbox", "Placeholder", function(Text)
+print("TextBox Triggered: " .. Text)
 end)
-Section1:Keybind({
-    Title = "Kill All",
-    Description = "",
-    Default = Enum.KeyCode.Q,
-    }, function(value)
-    print(value)
+Tab1.Folder("Lipsum Expanded", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eu mollis urna, quis feugiat tellus. Integer ut ligula sodales, sodales ipsum ut, imperdiet ipsum. In aliquet quam et venenatis pulvinar. Nullam fermentum porta felis sit amet interdum. Sed tristique fringilla mollis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam quis tempus mauris, nec ultrices metus. Suspendisse mi urna, accumsan at nisi a, tristique porta libero. Integer lobortis elementum lacus cursus consectetur. Morbi mauris ante, posuere at malesuada et, tristique non ipsum. Proin vitae purus pretium, convallis est vitae, dignissim leo. Praesent nec felis vitae.")
+
+local Cheat = Tab1.Cheat("Options", "A bunch of options you can use", function(Status)
+print("Cheat Triggered: " .. tostring(Status))
+end)
+Cheat.Button("Button", function()
+print("Button Clicked")
+end)
+Cheat.Switch("Switch", function(Status)
+print("Switch Triggered: " .. tostring(Status))
+end)
+Cheat.Toggle("Toggle", function(Status)
+print("Toggle Triggered: " .. tostring(Status))
+end)
+Cheat.Toggle("Toggle", function(Status)
+print("Toggle Triggered: " .. tostring(Status))
+end)
+Cheat.TextBox("Textbox", "Placeholder", function(Text)
+print("TextBox Triggered: " .. Text)
+end)
+
+Tab1.Cheat("Lipsum Expanded", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eu mollis urna, quis feugiat tellus. Integer ut ligula sodales, sodales ipsum ut, imperdiet ipsum. In aliquet quam et venenatis pulvinar. Nullam fermentum porta felis sit amet interdum. Sed tristique fringilla mollis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam quis tempus mauris, nec ultrices metus. Suspendisse mi urna, accumsan at nisi a, tristique porta libero. Integer lobortis elementum lacus cursus consectetur. Morbi mauris ante, posuere at malesuada et, tristique non ipsum. Proin vitae purus pretium, convallis est vitae, dignissim leo. Praesent nec felis vitae.", function(Status)
+print("Cheat Triggered: " .. tostring(Status))
+end)
+
+-- Tab 2 --
+local Tab2 = Window.Tab("Tab 2", 6022668945)
+local Folder = Tab2.Folder("Options", "A bunch of options you can use")
+Folder.Button("Button", function()
+print("Button Clicked")
+end)
+Folder.Switch("Switch", function(Status)
+print("Switch Triggered: " .. tostring(Status))
+end)
+Folder.Toggle("Toggle", function(Status)
+print("Toggle Triggered: " .. tostring(Status))
+end)
+Folder.Toggle("Toggle", function(Status)
+print("Toggle Triggered: " .. tostring(Status))
+end)
+Folder.TextBox("Textbox", "Placeholder", function(Text)
+print("TextBox Triggered: " .. Text)
+end)
+Tab2.Folder("Lipsum Expanded", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eu mollis urna, quis feugiat tellus. Integer ut ligula sodales, sodales ipsum ut, imperdiet ipsum. In aliquet quam et venenatis pulvinar. Nullam fermentum porta felis sit amet interdum. Sed tristique fringilla mollis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam quis tempus mauris, nec ultrices metus. Suspendisse mi urna, accumsan at nisi a, tristique porta libero. Integer lobortis elementum lacus cursus consectetur. Morbi mauris ante, posuere at malesuada et, tristique non ipsum. Proin vitae purus pretium, convallis est vitae, dignissim leo. Praesent nec felis vitae.")
+
+local Cheat = Tab2.Cheat("Options", "A bunch of options you can use", function(Status)
+print("Cheat Triggered: " .. tostring(Status))
+end)
+Cheat.Button("Button", function()
+print("Button Clicked")
+end)
+Cheat.Switch("Switch", function(Status)
+print("Switch Triggered: " .. tostring(Status))
+end)
+Cheat.Toggle("Toggle", function(Status)
+print("Toggle Triggered: " .. tostring(Status))
+end)
+Cheat.Toggle("Toggle", function(Status)
+print("Toggle Triggered: " .. tostring(Status))
+end)
+Cheat.TextBox("Textbox", "Placeholder", function(Text)
+print("TextBox Triggered: " .. Text)
+end)
+
+Tab2.Cheat("Lipsum Expanded", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eu mollis urna, quis feugiat tellus. Integer ut ligula sodales, sodales ipsum ut, imperdiet ipsum. In aliquet quam et venenatis pulvinar. Nullam fermentum porta felis sit amet interdum. Sed tristique fringilla mollis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam quis tempus mauris, nec ultrices metus. Suspendisse mi urna, accumsan at nisi a, tristique porta libero. Integer lobortis elementum lacus cursus consectetur. Morbi mauris ante, posuere at malesuada et, tristique non ipsum. Proin vitae purus pretium, convallis est vitae, dignissim leo. Praesent nec felis vitae.", function(Status)
+print("Cheat Triggered: " .. tostring(Status))
+end)
+
+game:GetService("UserInputService").InputBegan:Connect(function(Input)
+if Input.KeyCode == Enum.KeyCode.F then
+Window:Toggle()
+end
 end)
