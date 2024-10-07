431acc59653a747095241c10a85354bbde3a5cbc
FeHari <alexandregomesap202@gmail.com>
2024-10-07 00:11:17 -0300

    FH.lua

--- FH.lua
+++ FH.lua
@@ -1,28 +1,119 @@
-local Flux = loadstring(game:HttpGet"https://raw.githubusercontent.com/FeHariHub/FarmNovo/main/LibUI.lua")()
+local GUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/FeHariHub/FarmNovo/main/LibUI.lua"))()
 
-local win = Flux:Window("FeHari Hub", "Lendas Da Velocidade ⚡", Color3.fromRGB(255, 110, 48), Enum.KeyCode.LeftControl)
-local tab = win:Tab("Tab 1", "http://www.roblox.com/asset/?id=6023426915")
-tab:Button("Kill all", "This function may not work sometimes and you can get banned.", function()
-Flux:Notification("Killed all players successfully!", "Alright")
+local UI = GUI:CreateWindow("the um","lop")
+
+local Home = UI:addPage("Home",1,true,6)
+
+Home:addLabel("This is a Label","Lol this funny")
+
+Home:addButton("This is a button",function()
+    game.StarterGui:SetCore("SendNotification",{
+        Title = "Clicked";
+        Text = "Lo";
+    })
 end)
-tab:Label("This is just a label.")
-tab:Line()
-tab:Toggle("Auto-Farm Coins", "Automatically collects coins for you!", function(t)
-print(t)
+
+Home:addToggle("This is a Toggle",function(value)
+    print(value)
+    if value == false then 
+        game.StarterGui:SetCore("SendNotification",{
+            Title = "Toggle";
+            Text = "false";
+        })
+    else 
+        game.StarterGui:SetCore("SendNotification",{
+            Title = "Toggle";
+            Text = "true";
+        })
+    end
 end)
-tab:Slider("Walkspeed", "Makes your faster.", 0, 100,16,function(t)
-print(t)
+
+Home:addSlider("This is a Slider",16,100,function(value)
+    print(value)
 end)
-tab:Dropdown("Part to aim at", {"Torso","Head","Penis"}, function(t)
-print(t)
+
+Home:addTextBox("This is a TextBox","Um",function(value)
+    game.StarterGui:SetCore("SendNotification",{
+        Title = "Wrote";
+        Text = value;
+    })
 end)
-tab:Colorpicker("ESP Color", Color3.fromRGB(255,1,1), function(t)
-print(t)
+
+Home:addDropdown("This is a Dropdown",{"Um","Yep","Lop","GG"},1,function(value)
+    game.StarterGui:SetCore("SendNotification",{
+        Title = "Selected :";
+        Text = value;
+    }) 
 end)
-tab:Textbox("Gun Power", "This textbox changes your gun power, so you can kill everyone faster and easier.", true, function(t)
-print(t)
+
+-- Just an example of how you would actually use it i guess
+
+local LP = UI:addPage("Local",2,false,6)
+
+-- Label
+
+LP:addLabel("Local","Don't use in games with anti cheats")
+
+--- Button
+
+LP:addButton("DIE",function()
+    game.Players.LocalPlayer.Character.Humanoid.Health = 0
 end)
-tab:Bind("Kill Bind", Enum.KeyCode.Q, function()
-print("Killed a random person!")
+
+-- Toggle
+
+LP:addToggle("Sprint",function(value)
+    if value == false then
+        game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 16
+    else
+        game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 24
+    end
 end)
-win:Tab("Tab 2", "http://www.roblox.com/asset/?id=6022668888")
+
+-- Slider
+
+LP:addSlider("WalkSpeed",16,150,function(value)
+    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = value
+end)
+
+-- Textbox
+
+LP:addTextBox("Jump Power / 50 is default","Number here",function(value)
+    game.Players.LocalPlayer.Character.Humanoid.JumpPower = value
+end)
+
+-- Dropdown 
+
+local PLIST = {}
+
+for i,v in pairs(game:GetService("Players"):GetPlayers()) do
+    table.insert(PLIST,v.DisplayName)
+end
+
+LP:addDropdown("Teleport to Player",PLIST,4,function(value)
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame =  game.Players[value].Character.HumanoidRootPart.CFrame * CFrame.new(0,2,1)
+end)
+
+
+-- Spam 
+
+LP:addLabel("Spam","This is just to show how it looks with more ui elements")
+
+LP:addDropdown("Teleport to Player",PLIST,4,function(value)
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame =  game.Players[value].Character.HumanoidRootPart.CFrame * CFrame.new(0,2,1)
+end)
+
+
+LP:addDropdown("Teleport to Player",PLIST,4,function(value)
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame =  game.Players[value].Character.HumanoidRootPart.CFrame * CFrame.new(0,2,1)
+end)
+
+
+LP:addDropdown("Teleport to Player",PLIST,4,function(value)
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame =  game.Players[value].Character.HumanoidRootPart.CFrame * CFrame.new(0,2,1)
+end)
+
+
+LP:addDropdown("Teleport to Player",PLIST,4,function(value)
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame =  game.Players[value].Character.HumanoidRootPart.CFrame * CFrame.new(0,2,1)
+end)
