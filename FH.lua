da6193f3ae1b91a7fb0cd83cada8ecaaf482a27e
FeHari <alexandregomesap202@gmail.com>
2024-10-06 21:47:38 -0300

    FH.lua

--- FH.lua
+++ FH.lua
@@ -3,7 +3,7 @@
 -- Olá, e bem-vindo à documentação! Este documento cobrirá brevemente todos os passos necessários para criar uma interface de usuário (UI). :)
 
 -- Variável Principal
-local library = {loadstring(game:HttpGet("https://raw.githubusercontent.com/FeHariHub/FarmNovo/main/LibUI.lua", true))()} -- Defina a variável library aqui
+local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/FeHariHub/FarmNovo/main/LibUI.lua", true))() -- Defina a variável library aqui
 
 -- Mapa de Funções
 library = {
